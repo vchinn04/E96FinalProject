e15bd46bd1585a47a5118e327019d80a3fa76818
Erica Carballo <airwreckuh101@gmail.com>
2024-03-13 05:40:28 -0700

    Hearts work and player can now die successfully

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,16 +5,26 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] Transform target;
     NavMeshAgent agent;
+    PlayerHealth ph;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
+        ph = GameObject.Find("Player").GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         agent.SetDestination(target.position);
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            ph.TakeDamage(1);
+        }
     }
 }
